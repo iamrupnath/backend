39eb84c832a121169c0f8bf157a225dd5d2b03ed
iamrupnath <change.01life@gmail.com>
2024-02-24 21:07:31 +0530

    Referesh Tokens

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,7 +4,27 @@ import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { ApiError } from "../utils/ApiError.js";
 
+const generateAccessAndRefereshTokens = async(userId) =>{
+  try {
+      const user = await User.findById(userId)
+      const accessToken = user.generateAccessToken()
+      const refreshToken = user.generateRefreshToken()
+
+      user.refreshToken = refreshToken
+      await user.save({ validateBeforeSave: false })
+
+      return {accessToken, refreshToken}
+
+
+  } catch (error) {
+      throw new ApiError(500, "Something went wrong while generating referesh and access token")
+  }
+}
+
+
+
 const registerUser = asyncHandler(async (req, res) => {
+  // 👇👇 Register To-Do
   // get user details from frontend
   // validation - not empty
   // check if user already exists: username, email
@@ -78,4 +98,37 @@ const registerUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, createdUser, "User registered Successfully"));
 });
 
-export { registerUser };
+const loginUser = asyncHandler(async (req, res) => {
+
+  // 👇👇 Login To-Do
+  // req body -> data
+  // username or email
+  // find the user
+  // password check
+  // access and referesh token
+  // send cookie
+
+  const { email, username, password } = req.body;
+  console.log(email);
+
+  if (!username || !email) {
+    throw new ApiError(400, "username or eamil is required");
+  }
+
+  const user = User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (!user) {
+    throw new ApiError(404, "User does not exist")
+}
+
+const isPasswordValid = await user.isPasswordCorrect(password)
+
+if (!isPasswordValid) {
+  throw new ApiError(401, "Invalid user credentials")
+  }
+
+});
+
+export { registerUser, loginUser };
